89f3cbd0c627e02a989665df34f2b2d17ca9ede3
Richard Ogden <ogden.richard@gmail.com>
2023-06-29 17:24:20 +0200

    29062023 | searchMatchingDropdownItems

--- js/mvc/RecipesModel.js
+++ js/mvc/RecipesModel.js
@@ -270,20 +270,12 @@ export default class RecipesModel {
    * @returns {Array}
    */
   static searchMatchingDropdownItems = (sourceArray, stringVal) => {
-    const searchResult = [];
-    let index = sourceArray.length;
-    while (index) {
-      index -= 1;
-      const item = sourceArray[index];
-      if (
-        RecipesModel.searchString(
-          item.name.toLowerCase(),
-          stringVal.toLowerCase()
-        )
-      ) {
-        searchResult.push(item);
-      }
-    }
+    const searchResult = sourceArray.filter((item) =>
+      RecipesModel.searchString(
+        item.name.toLowerCase(),
+        stringVal.toLowerCase()
+      )
+    );
     return searchResult;
   };
 
